80f8f981d078b66cbb1ab887be345c871c3e04ab
Abhishek Shukla <90856267+abhishek2002Git@users.noreply.github.com>
2022-09-22 20:50:33 +0530

    types of recursion

---
+++ Lec3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+// Parameterised Recursion
+void sum1toN(int i, int n, int sum = 0)
+{
+    if (i > n)
+    {
+        cout << sum << endl;
+        return;
+    }
+
+    sum1toN(i + 1, n, sum + i);
+}
+
+// Functional Recursion
+int sum1toNFunc(int n)
+{
+    if (n == 0)
+        return 0;
+
+    return n + sum1toNFunc(n - 1);
+}
+
+int main()
+{
+    // sum1toN(1, 5);
+    cout<<sum1toNFunc(4);
+    return 0;
+}
